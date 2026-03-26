dbe33f4d3a786f2f2dfdd406238ca020e57ce2bd
Maria Alejandra <malejab2409@gmail.com>
2026-03-25 20:18:59 -0500

    Problema 1

--- dict_concept.py
+++ dict_concept.py
@@ -1,1 +1,10 @@
-# Write your code here!
+def get_config():
+    return
+
+diccionario = {
+    "width": 480,
+    "height": 480,
+    "color_mode": "dark" 
+    "sensitivity" "0.4"
+    }
+print (diccionario.items())
