86b71de2c07b2c36983a300965cae73a22866293
Maria Alejandra <malejab2409@gmail.com>
2026-03-26 19:35:00 -0500

    Problema 2

--- dict_store.py
+++ dict_store.py
@@ -1,6 +1,6 @@
 def temp_and_color(data):
-  dict = {
-    "temperature" : 22.5,
-    "color" : "blue"
-  }
-  return
+  temperatura = data.get("temp")
+  color = data.get("color")
+  return (temperatura, color)
+data = {"temp": 22.5, "color": "blue", "status": "ok"}
+print(temp_and_color(data)) 
